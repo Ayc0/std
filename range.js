79549bcd8fc9a44ccd7f56f4c8be5ee59588b4fd
Ayc0 <ayc0.benj@gmail.com>
2018-12-23 22:23:29 +0100

    Create range.js

---
+++ range.js
@@ -0,0 +1,30 @@
+function* range() {
+  let from = 0;
+  let to = 0;
+  let step = 1;
+  if (arguments.length == 1) {
+    to = arguments[0];
+  } else if (arguments.length >= 2) {
+    from = arguments[0];
+    to = arguments[1];
+  }
+  if (arguments.length >= 3) {
+    step = arguments[2];
+  }
+  if (step === 0) {
+    return
+  }
+  if (step > 0) {
+    while (from < to) {
+      yield from;
+      from += step;
+    }
+   } else {
+    while (from > to) {
+      yield from;
+      from += step;
+    }
+  }
+}
+
+module.exports = range
