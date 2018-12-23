e3a547029bc2ef7a0dc89e145dce58522855f482
Ayc0 <ayc0.benj@gmail.com>
2018-12-23 23:30:00 +0100

    Create forEach.js

---
+++ forEach.js
@@ -0,0 +1,9 @@
+const iterate = require('./iterate');
+
+const forEach = (iterable, callback, thisArg) => {
+	for (let step of iterate(iterable)) {
+	  callback.apply(thisArg, step);
+  }
+};
+
+module.exports = forEach;
