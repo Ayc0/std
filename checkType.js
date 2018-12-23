daf5854819278942966238e32885e7e0d324cf52
Ayc0 <ayc0.benj@gmail.com>
2018-12-23 23:19:28 +0100

    Update checkType.js

--- checkType.js
+++ checkType.js
@@ -23,38 +23,6 @@ const checkType = element => {
   return null;
 }
 
-function* iterate(iterable) {
-  const type = checkType(iterable);
-  if (type === ARRAY || type === STRING) {
-    let index = 0;
-    while (index < iterable.length) {
-      yield [iterable[index], index, iterable];
-      index += 1;
-    }
-    return;
-  }
-  if (type === MAP) {
-    for (let key of iterable.keys()) {
-      yield [iterable.get(key), key, iterable];
-    }
-    return;
-  }
-  if (type === SET) {
-    for (let element of iterable) {
-      yield [element, null, iterable];
-    }
-    return;
-  }
-  if (type === OBJECT) {
-    for (let key in iterable) {
-      if (iterable.hasOwnProperty(key)) {
-        yield [iterable[key], key, iterable];
-      }
-    }
-    return;
-  }
-}
-
 module.exports = {
   checkType,
   type: {
