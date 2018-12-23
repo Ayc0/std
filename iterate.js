3891d671e2017063676ff35daf2fb3ffd7edf8cd
Ayc0 <ayc0.benj@gmail.com>
2018-12-23 22:43:21 +0100

    check types

---
+++ iterate.js
@@ -0,0 +1,35 @@
+const ARRAY = Symbol('Array');
+const STRING = Symbol('String');
+const SET = Symbol('Set');
+const MAP = Symbol('Map');
+const OBJECT = Symbol('Object');
+
+const checkType = element => {
+  if (typeof element === 'string' || element instanceof String) {
+    return STRING;
+  }
+  if (Array.isArray(element)) {
+    return ARRAY;
+  }
+  if (element instanceof Set) {
+    return SET;
+  }
+  if (element instanceof Map) {
+    return MAP;
+  }
+  if (typeof element === 'object' || element instanceof Object) {
+    return OBJECT;
+  }
+  return null;
+}
+
+module.exports = {
+  checkType,
+  type: {
+    Array: ARRAY,
+    String: STRING,
+    Set: SET,
+    Map: MAP,
+    Object: OBJECT,
+  }
+}
