cf8c0e734d35baf43b7edf0d9766309cc00c1e76
Ayc0 <ayc0.benj@gmail.com>
2018-12-24 01:36:08 +0100

    map

---
+++ map.js
@@ -0,0 +1,15 @@
+const iterate = require("./iterate");
+const { checkType } = require("./checkType");
+const generateIterable = require("./generateIterable");
+
+function* apply(iterable, callback, thisArg) {
+  for (let step of iterate(iterable)) {
+    yield [callback.apply(thisArg, step), step[1]];
+  }
+}
+const map = (iterable, callback, thisArg) => {
+  const type = checkType(iterable);
+  return generateIterable(apply(iterable, callback, thisArg), type);
+};
+
+module.exports = map;
